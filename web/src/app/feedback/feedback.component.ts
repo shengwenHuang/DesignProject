44f17ef80ba5838a936ecbcdf4d607bbe4ce9d39
Alice <sheng-wen.huang.18@ucl.ac.uk>
2019-02-25 23:28:26 +0000

    feedback dashboard

--- web/src/app/feedback/feedback.component.ts
+++ web/src/app/feedback/feedback.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import * as CanvasJS from 'canvasjs.min';
+import { Chart} from 'chart.js';
+// import * as CanvasJS from 'canvasjs.min';
 
 @Component({
     selector: 'app-feedback',
@@ -9,13 +10,12 @@ import * as CanvasJS from 'canvasjs.min';
 
 export class FeedbackComponent implements OnInit {
 
-  responses : any = [
-    {"Title": "How was your experience?", "A1": 5, "A2": 4 ,"A3": 3, "A4": 5, "A5": 2},
-    {"Title": "Do you like the app?", "A1": 3, "A2": 2 ,"A3": 3, "A4": 5, "A5": 6 },
-    {"Title": "Would you recommend to a friend?", "A1": 6, "A2": 1 ,"A3": 3, "A4": 5, "A5": 4 },
-    {"Title": "Are you happy with the NHS in general?", "A1": 5, "A2": 2 ,"A3": 3, "A4": 2, "A5": 0},
-    {"Title": "Do you have any more questions?", "A1": 3, "A2": 2 ,"A3": 1, "A4": 5, "A5": 2 },
-    {"Title": "Have you had a nice day?", "A1": 2, "A2": 6 ,"A3": 5, "A4": 10, "A5": 2},
+  responses: any = [
+    {"Title": "You signed in using the self-check in kiosk and found it easy to use?", "A1": 50, "A2": 40 ,"A3": 30, "A4": 50, "A5": 20},
+    {"Title": "The POAC waiting area was comfortable", "A1": 32, "A2": 22 ,"A3": 32, "A4": 52, "A5": 62 },
+    {"Title": "You were kept informed and received all the information/instructions you needed", "A1": 65, "A2": 15 ,"A3": 35, "A4": 55, "A5": 45 },
+    {"Title": "You were able to complete the screening questions on the iPad/mobile device", "A1": 57, "A2": 27 ,"A3": 37, "A4": 27, "A5": 7},
+    {"Title": "You would recommend our POAC service to friends and family", "A1": 32, "A2": 22 ,"A3": 12, "A4": 52, "A5": 22 },
   ]
 
   ngOnInit() {}
@@ -24,26 +24,69 @@ export class FeedbackComponent implements OnInit {
 
       var i = 0
       for (i = 0; i < this.responses.length; i++) {
-        var chart = new CanvasJS.Chart("chartContainer" + i, {
-          theme: "light1", // "light1", "light2", "dark1", "dark2"
-          animationEnabled: true,
-          title: {
-            text: this.responses[i].Title
-          },
-          data: [{
-            type: "column",
-            toolTipContent: "<b>{label}</b>: {y} Respondent(s)",
-            legendText: "{label}",
-            dataPoints: [
-              { y: this.responses[i].A1, label: "Strongly Disgree" },
-              { y: this.responses[i].A2, label: "Disagree" },
-              { y: this.responses[i].A3, label: "Neither" },
-              { y: this.responses[i].A4, label: "Agree" },
-              { y: this.responses[i].A5, label: "Strongly Agree" },
+
+        var chart = new Chart(document.getElementById("chartContainer" + i), {
+          type: "bar",
+          fontSize: 15,
+          data: {
+            labels: ["Strongly Disagree", "Disagree", "Neither", "Agree", "Strongly Agree"],
+            datasets: [
+              {
+                label: "# of Respondent(s)",
+                fontSize: 10,
+                data: [
+                  this.responses[i].A1,
+                  this.responses[i].A2,
+                  this.responses[i].A3,
+                  this.responses[i].A4,
+                  this.responses[i].A5
+                ],
+                backgroundColor: [
+                  'rgba(255,   0,   0, 0.4)',
+                  'rgba(255, 165,   0, 0.4)',
+                  'rgba(255, 255,   0, 0.4)',
+                  'rgba(  0, 255, 255, 0.4)',
+                  'rgba(  0, 255,   0, 0.4)'
+                  ],
+                borderColor: [
+                  'rgba(255,   0,   0, 1)',
+                  'rgba(255, 165,   0, 1)',
+                  'rgba(255, 255,   0, 1)',
+                  'rgba(  0, 255, 255, 1)',
+                  'rgba(  0, 255,   0, 1)'
+                  ],
+                borderWidth: 1,
+              }
             ]
-          }]
+          },
+          options: {
+            tooltips: {
+              titleFontSize: 15,
+              bodyFontSize: 15 },
+            scales: {
+              xAxes: [{
+                  ticks: {
+                      fontSize: 15,
+                      fontStyle: "bold"
+                  }
+              }],
+              yAxes: [{
+                ticks: {
+                    fontSize: 15,
+                    beginAtZero: true,
+                    callback: function(value) {if (value % 1 === 0) {return value;}}
+                }
+            }]
+          },
+            legend: { display: false },
+            title: {
+              display: true,
+              text: this.responses[i].Title,
+              fontSize: 20
+            }
+          }
         });
-        chart.render();
+
       }
 
     }
