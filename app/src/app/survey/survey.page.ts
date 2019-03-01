dc7259e4c5242fe93d23cb9b6dc49acf57d77ed8
nishchalsen <37885719+nishchalsen@users.noreply.github.com>
2019-03-01 22:46:13 +0000

    Update survey.page.ts

--- app/src/app/survey/survey.page.ts
+++ app/src/app/survey/survey.page.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service';
 import { AlertController } from '@ionic/angular';
@@ -71,15 +72,13 @@ export class SurveyPage implements OnInit {
   submit() {
     
     for (let q of this.all_question) {
-      if (q.type === "free text") {
-        console.log()
-      }
+
       if (!q.answers) {
         this.displayAlert("Please complete the survey", "Please answer all the question.");
         return;
       }
-      this.displayAlert("Success", "Thank you for filling in the survey")
     }
+    this.displayAlert("Success", "Thank you for filling in the survey")
   } 
 
 
@@ -96,3 +95,101 @@ export class SurveyPage implements OnInit {
 
 }
 					
+=======
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
+import { AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-survey',
+  templateUrl: './survey.page.html',
+  styleUrls: ['./survey.page.scss'],
+})
+export class SurveyPage implements OnInit {
+
+  all_question: any
+
+
+    
+  constructor(
+    private apiService: ApiService,
+    private alert: AlertController) {  }
+
+  
+
+  ngOnInit() {
+    this.all_question = [
+      {
+          "questionID": 1,
+          "question": "Allergies / Adverse reactions to food or medication?",
+          "type": "free text",
+          "pos": 1,
+          "answers": ""
+      },
+      {
+          "questionID": 2,
+          "question": "Allergies",
+          "type": "yes/no",
+          "pos": 2,
+          "answers": ""
+      },
+      {
+          "questionID": 3,
+          "question": "Adverse reaction",
+          "type": "free text",
+          "pos": 3,
+          "answers": ""
+      },
+      {
+          "questionID": 4,
+          "question": "Testing ... -_-",
+          "type": "yes/no",
+          "pos": 4,
+          "answers": ""
+      }]
+
+    // this.apiService.getQuestions()
+    // .subscribe(
+    //   (data) => this.all_question = data,
+    //   (err) => console.log(err)
+    // )
+
+  }
+  yes(pos: number) {
+    this.all_question[pos -1].answers = "yes"; 
+    console.log(pos)
+    console.log(this.all_question[pos -1])
+  }
+
+  no(pos: number) {
+    this.all_question[pos -1].answers = "no"; 
+    console.log(this.all_question[pos -1])
+  }
+
+  submit() {
+    
+    for (let q of this.all_question) {
+
+      if (!q.answers) {
+        this.displayAlert("Please complete the survey", "Please answer all the question.");
+        return;
+      }
+    }
+    this.displayAlert("Success", "Thank you for filling in the survey")
+  } 
+
+
+  async displayAlert(header: string, message: string) {
+    const alert = await this.alert.create({
+      header,
+      message,
+      buttons: ["Ok"]
+    })
+
+    await alert.present()
+
+  }
+
+}
+					
+>>>>>>> 5615d1b1616a2ead442715d417a69cc7972690a9
