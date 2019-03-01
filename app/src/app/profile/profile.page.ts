f59987b71b674152e583c559191d56f74ce4b7aa
nishchalsen <37885719+nishchalsen@users.noreply.github.com>
2019-03-01 22:46:38 +0000

    Update profile.page.ts

--- app/src/app/profile/profile.page.ts
+++ app/src/app/profile/profile.page.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service';
 
@@ -8,7 +9,14 @@ import { ApiService } from '../services/api.service';
 })
 export class ProfilePage implements OnInit {
 
-  userData: object;
+  userData: object= {
+    NHSno : "",
+    firstname: "",
+    lastname: "",
+    email:"",
+    phone:""
+  };
+  
   constructor(
     private apiService: ApiService
   ) { }
@@ -22,3 +30,36 @@ export class ProfilePage implements OnInit {
   }
 
 }
+=======
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.page.html',
+  styleUrls: ['./profile.page.scss'],
+})
+export class ProfilePage implements OnInit {
+
+  userData: object= {
+    NHSno : "",
+    firstname: "",
+    lastname: "",
+    email:"",
+    phone:""
+  };
+  
+  constructor(
+    private apiService: ApiService
+  ) { }
+
+  ngOnInit() {
+    this.apiService.getUser()
+    .subscribe(
+      (data) => {this.userData = data, console.log(this.userData)},
+      (err) => console.log(err)
+    )
+  }
+
+}
+>>>>>>> 5615d1b1616a2ead442715d417a69cc7972690a9
