18f4cc5d4f98baa0152a15ebf5c2deb803af55e5
nishchalsen <37885719+nishchalsen@users.noreply.github.com>
2019-04-14 08:02:38 +0100

    Update navigation.component.ts

--- web/src/app/navigation/navigation.component.ts
+++ web/src/app/navigation/navigation.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service'
-import { Router } from '@angular/router';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
